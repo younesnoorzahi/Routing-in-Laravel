fe24f3757ac0cb295591a3f2179ce278d22cb757
Younes <younes.noorzai2005@gmail.com>
2025-07-21 15:19:30 +0330

    Delete POST، PUT، DELETE.php

--- POST، PUT، DELETE.php
+++
@@ -1,3 +0,0 @@
-Route::post('/submit', [FormController::class, 'submit']);
-Route::put('/update/{id}', [UserController::class, 'update']);
-Route::delete('/delete/{id}', [UserController::class, 'destroy']);

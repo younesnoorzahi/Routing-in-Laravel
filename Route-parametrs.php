4297bc9527cdeca5433aa31367b5190cb24002f0
Younes <younes.noorzai2005@gmail.com>
2025-07-21 15:18:45 +0330

    Update Route-parametrs.php

--- Route-parametrs.php
+++ Route-parametrs.php
@@ -1,3 +1,8 @@
 Route::get('/user/{id}', function ($id) {
     return 'User ID: ' . $id;
 });
+
+<!-- پارامتر اختیاری -->
+Route::get('/user/{name?}', function ($name = 'Guest') {
+    return $name;
+});

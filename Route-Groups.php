9a5d19bf8293140a6b2affc85ffdf665311d7e01
Younes <younes.noorzai2005@gmail.com>
2025-07-21 15:25:15 +0330

    Delete Route-Groups.php

--- Route-Groups.php
+++
@@ -1,4 +0,0 @@
-Route::prefix('admin')->middleware('auth')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'dashboard']);
-    Route::get('/users', [AdminController::class, 'users']);
-});
